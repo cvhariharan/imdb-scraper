bd6cf3d955a813d1c0ce9e1bf61f0edf9783c6cd
Hariharan <theroboticlabs@gmail.com>
2017-10-02 18:32:19 +0530

    Added website class. Can be used a module.

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -1,26 +1,31 @@
 import requests,sys
+class website:
+    url = ""
+    def __init__(self,url):
+        self.url = url
 
-imdb_url = sys.argv[1]
-html_source = requests.get(imdb_url).text
+    def poster(self):
+        imdb_url = self.url
+        html_source = requests.get(imdb_url).text
+        #Find the poster division and isolate it from the rest of the html source
+        poster_content = self.snippet("<div class=\"poster\">","</div>",html_source,0)
+        #print(poster_content)
 
-def snippet(start,end,html_source,start_offset):
-    snippet_index_start = html_source.find(start)
-    snippet_index_end = html_source.find(end,snippet_index_start)
-    return html_source[snippet_index_start+start_offset:snippet_index_end]
+        #Find the url that points to the image
+        imgurl = self.snippet("src","itemprop",poster_content,len("src=")).replace("\n","").replace("\"","")
 
-#Find the poster division and isolate it from the rest of the html source
-poster_content = snippet("<div class=\"poster\">","</div>",html_source,0)
-print(poster_content)
+        #Change the image dimensions
+        imgurl=imgurl.replace("UX182_CR0,0,182,268_AL__QL50","UX400_CR0,0,400,585_AL__QL50") #Replace with @@._V1_QL50_SY1000_CR0,0,674,1000_AL_.jpg to directly get hd posters
 
-#Find the url that points to the image
-imgurl = snippet("src","itemprop",poster_content,len("src=")).replace("\n","").replace("\"","")
+        #Open a BINARY file and write the contents to it
+        #image = open(imdb_url.split("/")[4]+".jpg","wb")
+        image_data = requests.get(imgurl).content
+        #image.write(image_data)
+        #image.close()
+        #print(imgurl)
+        return imgurl,image_data
 
-#Change the image dimensions
-imgurl=imgurl.replace("UX182_CR0,0,182,268_AL__QL50","UX400_CR0,0,400,585_AL__QL50") #Replace with @@._V1_QL50_SY1000_CR0,0,674,1000_AL_.jpg to directly get hd posters
-
-#Open a BINARY file and write the contents to it
-image = open(imdb_url.split("/")[4]+".jpg","wb")
-image_data = requests.get(imgurl).content
-image.write(image_data)
-image.close()
-print(imgurl)
+    def snippet(self,start,end,html_source,start_offset):
+        snippet_index_start = html_source.find(start)
+        snippet_index_end = html_source.find(end,snippet_index_start)
+        return html_source[snippet_index_start+start_offset:snippet_index_end]
