7ab4433cbe19677e5574147860942a9eae452cff
Hariharan <theroboticlabs@gmail.com>
2017-10-02 22:57:55 +0530

    Improved readability.

--- scraping_example.py
+++ scraping_example.py
@@ -1,13 +1,13 @@
 import imdb_scraper,requests
-
-imdb = imdb_scraper.website("lego batman",True)
-#imgurl,image_data = imdb.poster()
+movie_name = input("Name of the Movie or TV Series: ")
+imdb = imdb_scraper.website(movie_name,True)
 plot = imdb.plot()
 rating = imdb.rating()
 title = imdb.title()
 imgurl = imdb.poster()
+#Save the poster
 image = open(title+".jpg","wb")
 image.write(requests.get(imgurl).content)
 image.close()
 review = imdb.reviews()
-print(title+"\n"+rating+"\n"+plot+"\n"+imgurl+"\n"+review)
+print("Title: "+title+"\n"+"Rating: "+rating+"\n"+"Plot: "+plot+"\n"+"Poster: "+imgurl+"\n"+"Review: "+review)
