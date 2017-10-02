f0074b813d64665f236cd720a7b8042c720c0060
Hariharan <theroboticlabs@gmail.com>
2017-10-02 19:58:24 +0530

    Downloading a poster.

--- scraping_example.py
+++ scraping_example.py
@@ -1,4 +1,4 @@
-import imdb_scraper
+import imdb_scraper,requests
 
 imdb = imdb_scraper.website("http://www.imdb.com/title/tt3014284/")
 #imgurl,image_data = imdb.poster()
@@ -6,4 +6,7 @@ plot = imdb.plot()
 rating = imdb.rating()
 title = imdb.title()
 imgurl = imdb.poster()
+image = open("image.jpg","wb")
+image.write(requests.get(imgurl).content)
+image.close()
 print(title+"\n"+rating+"\n"+plot+"\n"+imgurl)
